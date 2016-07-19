4fbcc70a91657199bc2b8e01fbf3600df5c03dac
Victorio Cebedo II <cebedo.vii@gmail.com>
2016-07-19 11:11:12 +0800

    Integrated with uploaded Jars.

--- ServiceAsAService/src/main/java/com/cebedo/serviceasaservice/handler/RunHandler.java
+++ ServiceAsAService/src/main/java/com/cebedo/serviceasaservice/handler/RunHandler.java
@@ -3,24 +3,26 @@ package com.cebedo.serviceasaservice.handler;
 import com.cebedo.serviceasaservice.manager.JarManager;
 import io.vertx.core.Handler;
 import io.vertx.core.http.HttpMethod;
+import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.http.HttpServerResponse;
 import io.vertx.ext.web.RoutingContext;
 
 public class RunHandler implements Handler<RoutingContext> {
 
     public static final HttpMethod HTTP_METHOD = HttpMethod.GET;
-    public static final String HTTP_PATH = "/run";
+    public static final String HTTP_PATH = "/run.*";
+
+    private static final String PARAM_PLUGIN = "plugin";
 
     @Override
     public void handle(RoutingContext event) {
+        HttpServerRequest request = event.request();
+        String pluginMapping = request.getParam(PARAM_PLUGIN);
+        String runResult = JarManager.getInstance().runJar(pluginMapping);
 
-        // This handler will be called for every request
         HttpServerResponse response = event.response();
         response.putHeader("content-type", "text/plain");
-        // String runResult = JarManager.getInstance().runJar(HTTP_PATH);
-
-        // Write to the response and end it
-        response.end("RUN HANDLER!");
+        response.end(runResult);
     }
 
 }
--- ServiceAsAService/src/main/java/com/cebedo/serviceasaservice/server/VertxServer.java
+++ ServiceAsAService/src/main/java/com/cebedo/serviceasaservice/server/VertxServer.java
@@ -2,6 +2,7 @@ package com.cebedo.serviceasaservice.server;
 
 import com.cebedo.serviceasaservice.Runner;
 import com.cebedo.serviceasaservice.handler.RunHandler;
+import com.cebedo.serviceasaservice.manager.JarManager;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.http.HttpServer;
 import io.vertx.ext.web.Router;
@@ -10,17 +11,41 @@ public class VertxServer extends AbstractVerticle {
 
     public static final int SERVER_PORT = 8080;
 
-    // Convenience method so you can run it in your IDE
+    /**
+     * Convenience function to start in IDE.
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         Runner.runExample(VertxServer.class);
     }
 
+    /**
+     * Load built-in JARs.
+     *
+     * @throws Exception
+     */
+    private static void loadBuiltInJars() throws Exception {
+        String jarURL = "file:///C:/Users/Vic/Documents/Vic/git/ServiceAsAService/ServiceAsAService/src/main/java/com/cebedo/serviceasaservice/sample/dist/sample.jar";
+        String className = "sample.TestPlugin";
+        JarManager jarManager = JarManager.getInstance();
+        jarManager.loadJar(jarURL, className);
+    }
+
+    /**
+     * Start the Vert.x Server.
+     */
     @Override
     public void start() {
+        try {
+            loadBuiltInJars();
+        } catch (Exception e) {
+            ;
+        }
+
         HttpServer server = vertx.createHttpServer();
         Router router = Router.router(vertx);
-        router.route(RunHandler.HTTP_METHOD, RunHandler.HTTP_PATH).handler(new RunHandler());
-
+        router.routeWithRegex(RunHandler.HTTP_METHOD, RunHandler.HTTP_PATH).handler(new RunHandler());
         server.requestHandler(router::accept).listen(SERVER_PORT);
     }
 }
